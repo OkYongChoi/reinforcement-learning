08c56535a315a4a5788d8201cdc08c78388b4566
burger-okd <ok9623@gmail.com>
2021-05-06 23:46:20 +0900

    add sac_agent.py

---
+++ sac_agent.py
@@ -0,0 +1,8 @@
+import torch
+import torch.nn.functional as F
+from torch.optim import Adam
+from model import GaussianPolicy, QNetwork
+
+class soft_actor_critic_agent(object):
+    def __init__():
+        pass
